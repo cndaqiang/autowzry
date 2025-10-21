c27dee86d0727cacc8a18683e34720b83cb2dcc3
cndaqiang <cndaqiang@gmail.com>
2025-10-21 09:42:18 +0800

    清理master分支: https://cndaqiang.github.io/wzry.doc/

--- push.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Program Files\Git\git-bash.exe" -c "echo last_modified = \\\"$(date +\"%%Y-%%m-%%d %%H:%%M:%%S\")\\\" > version.py"
-
-"C:\Program Files\Git\git-bash.exe" -c "git add . && git commit -m \"Automated commit by bot: $(date)\" && git push origin master"
--- run.bat
+++
@@ -1,3 +0,0 @@
-%USERPROFILE%\miniconda3\python.exe wzry.py  config.win.yaml
-:: 因为windows的计划任务，无法在脚本内部启动模拟器
-:: 可以取消控制模拟器，保持模拟器在后台
